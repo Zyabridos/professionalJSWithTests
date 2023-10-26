a385463dce983e75dfffb9dc9d389daaaf440783
Nina Zyabrina <zyabrina95@gmail.com>
2023-10-26 20:24:20 +0200

    calculateDistance tests r done

---
+++ JS: Abstraction with the help of data/points_on_coordinate_system/__tests__/points.test.js
@@ -0,0 +1,15 @@
+import calculateDistance from '../src/points.js';
+
+test('calculateDistance', () => {
+  const point1 = [0, 0];
+  const point2 = [3, 4];
+  expect(calculateDistance(point1, point2)).toStrictEqual(5);
+});
+
+test('calculateDistance2', () => {
+  const point1 = [0, 0];
+  const point2 = [0, 0];
+  expect(calculateDistance(point1, point2)).toStrictEqual(0);
+});
+
+// NODE_OPTIONS=--experimental-vm-modules npx jest
