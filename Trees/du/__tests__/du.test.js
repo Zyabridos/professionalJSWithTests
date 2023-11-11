f140faed4ef594959345643ba6e5f5c9d412ef8f
Nina Zyabrina <zyabrina95@gmail.com>
2023-11-11 16:44:37 +0100

    hand-written command "du"

--- Trees/du/__tests__/du.test.js
+++ Trees/du/__tests__/du.test.js
@@ -1,1 +1,36 @@
+// @ts-check
+
+import { mkdir, mkfile, getChildren } from '../../VisualFileSystem/src/index.js';
+import du from '../src/du.js';
+
+const tree = mkdir('/', [
+  mkdir('etc', [
+    mkdir('apache'),
+    mkdir('nginx', [
+      mkfile('nginx.conf', { size: 800 }),
+    ]),
+    mkdir('consul', [
+      mkfile('config.json', { size: 1200 }),
+      mkfile('data', { size: 8200 }),
+      mkfile('raft', { size: 80 }),
+    ]),
+  ]),
+  mkfile('hosts', { size: 3500 }),
+  mkfile('resolve', { size: 1000 }),
+]);
+
+test('du', () => {
+  expect(du(tree)).toEqual([
+    ['etc', 10280],
+    ['hosts', 3500],
+    ['resolve', 1000],
+  ]);
+
+  expect(du(getChildren(tree)[0])).toEqual([
+    ['consul', 9480],
+    ['nginx', 800],
+    ['apache', 0],
+  ]);
+});
+
 // NODE_OPTIONS=--experimental-vm-modules npx jest
--- Trees/du/src/du.js
+++ Trees/du/src/du.js
@@ -1,49 +1,28 @@
 import _ from 'lodash';
 import {
-  isFile, getName, getMeta, getChildren, mkdir, mkfile, isDirectory,
+  isFile, getName, getMeta, getChildren, mkdir, mkfile,
 } from '../../VisualFileSystem/src/index.js';
 
-const compressImages = (tree) => {
-  const clone = _.cloneDeep(tree);
-  const { children } = clone;
-  const images = children.filter((files) => files.name.includes('.jpg'));
-  const halfSized = images.map((sizes) => sizes.meta.size / 2);
-  for (let i = 0; i < images.length; i += 1) {
-    images[i].meta.size = halfSized[i];
-  }
-  return clone;
-};
-const getSize = (tree) => {
-  const clone = _.cloneDeep(tree);
-  const { children } = clone;
-  const name = getName(tree);
-  const newMeta = _.cloneDeep(getMeta(tree));
-  const sum = children.reduce((acc, current) => acc + current.size, 0);
-  return newMeta;
-};
-
-const getFilesCount = (node) => {
-  if (isFile(node)) {
-    return 1;
-  }
-
+const getMetaCount = (node) => {
   const children = getChildren(node);
-  const descendantCounts = children.map(getFilesCount);
+  const meta = getMeta(node);
+  if (isFile(node)) {
+    return meta.size;
+  }
+
+  const descendantCounts = children.map(getMetaCount);
   return _.sum(descendantCounts);
 };
 
-const du = (tree) => {
+export default (tree) => {
   const children = getChildren(tree);
   const result = children
-    // Нас интересуют только директории
-    .filter(isDirectory)
-    // Запускаем подсчёт для каждой директории
-    .map((child) => [getName(child), getFilesCount(child)]);
-
+    .map((child) => [getName(child), getMetaCount(child)]);
+  result.sort(([, size1], [, size2]) => size2 - size1);
   return result;
 };
 
-const example = mkdir('/', [
+const tree = mkdir('/', [
   mkdir('etc', [
     mkdir('apache'),
     mkdir('nginx', [
@@ -58,11 +37,7 @@ const example = mkdir('/', [
   mkfile('hosts', { size: 3500 }),
   mkfile('resolve', { size: 1000 }),
 ]);
-
-console.log(getSize(example));
-// console.log(du(example));
-// [
-//   ['etc', 10280],
-//   ['hosts', 3500],
-//   ['resolve', 1000],
-// ]
+console.log(getMetaCount(getChildren(tree)[0]));
+console.log(du(getChildren(tree)[0]));
+console.log(getMetaCount(tree));
+console.log(du(tree));
