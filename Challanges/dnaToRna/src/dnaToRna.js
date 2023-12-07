6ff91a02500fd4c10b1cbf8058d09436c103d447
Nina Zyabrina <zyabrina95@gmail.com>
2023-12-07 18:02:49 +0100

    dnaToRna is completed

--- Challanges/dnaToRna/src/dnaToRna.js
+++ Challanges/dnaToRna/src/dnaToRna.js
@@ -1,6 +1,26 @@
-it('should work', () => {
-  expect(dnaToRna('ACGTGGTCTTAA')).toEqual('UGCACCAGAAUU');
-  expect(dnaToRna('CCGTA')).toEqual('GGCAU');
-  expect(dnaToRna('')).toEqual('');
-  expect(dnaToRna('ACNTG')).toEqual(null);
-});
+export default (str) => {
+  const myArray = str.split('');
+  const result = [];
+  myArray.map((current) => {
+    switch (current) {
+      case 'A':
+        result.push('U');
+        break;
+      case 'C':
+        result.push('G');
+        break;
+      case 'G':
+        result.push('C');
+        break;
+      case 'T':
+        result.push('A');
+        break;
+      default:
+        return 'null';
+    }
+  });
+  if (myArray.length !== result.length) {
+    return null;
+  }
+  return result.join('');
+};
