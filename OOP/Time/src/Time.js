f4e9f5e8ab03be1e1c0fcb6df623cc4b9addc91a
Nina Zyabrina <zyabrina95@gmail.com>
2023-12-27 18:30:14 +0100

    Circle

--- OOP/Time/src/Time.js
+++ OOP/Time/src/Time.js
@@ -1,23 +1,28 @@
 // @ts-check
 
 export default class Time {
+  // BEGIN (write your solution here)
+
+  // END
+
   constructor(hours, minutes) {
     this.minutes = minutes;
     this.hours = hours;
   }
 
-  fromString() {
+  toString() {
     return `${this.hours}:${this.minutes}`;
   }
 
-  toString() {
-    return `${this.hours}:${this.minutes}`;
+  fromString(str) {
+    const hours = this.toString();
+    return hours + str;
   }
 }
 
 const time = new Time(10, 15);
-console.log(`The time is ${time}`); // => 'The time is 10:15'
+// console.log(`The time is ${time}`); // => 'The time is 10:15'
 
-const time2 = Time.fromString('10:23');
+time.fromString('10:23');
 // автоматически вызывается метод toString()
-console.log(`The time is ${time2}`); // 'The time is 10:23'
+console.log(time); // 'The time is 10:23'
