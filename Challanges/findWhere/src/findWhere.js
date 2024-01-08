c492d89393c9d9964cdab869ad5f36c7b9ffad26
Nina Zyabrina <zyabrina95@gmail.com>
2024-01-08 15:39:09 +0100

    findWhere

--- Challanges/findWhere/src/findWhere.js
+++ Challanges/findWhere/src/findWhere.js
@@ -1,21 +1,18 @@
-const findWhere = (array, searchedItem) => {
-  const arrayEntries = Object.entries(array);
-  const searchedEntries = Object.entries(searchedItem);
-  // return arrayEntries;
-  // return searchedEntries[0][0];
-  // return array.map((current) => current.title);
-};
+const findWhere = (data, where) => {
+  const entries = Object.entries(where);
+  for (const item of data) {
+    let find = true;
+    for (const [key, value] of entries) {
+      if (item[key] !== value) {
+        find = false;
+      }
+    }
+    if (find) {
+      return item;
+    }
+  }
 
-console.log(findWhere(
-  [
-    { title: 'Book of Fooos', author: 'FooBar', year: 1111 },
-    { title: 'Cymbeline', author: 'Shakespeare', year: 1611 },
-    { title: 'The Tempest', author: 'Shakespeare', year: 1611 },
-    { title: 'Book of Foos Barrrs', author: 'FooBar', year: 2222 },
-    { title: 'Still foooing', author: 'FooBar', year: 3333 },
-    { title: 'Happy Foo', author: 'FooBar', year: 4444 },
-  ],
-  { author: 'Shakespeare', year: 1611 },
-)); // { title: 'Cymbeline', author: 'Shakespeare', year: 1611 }
+  return null;
+};
 
 export default findWhere;
