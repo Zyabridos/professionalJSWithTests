fb52704b52d061056595bbe56c882836b3360ba4
Nina Zyabrina <zyabrina95@gmail.com>
2024-01-15 18:11:41 +0100

    move file

--- asynchronous_programming/compareFileSizes/src/compareFileSizes.js
+++ asynchronous_programming/compareFileSizes/src/compareFileSizes.js
@@ -1,75 +1,22 @@
 import * as fs from 'fs';
 import path from 'path';
 
-const getFileOwners = (dirpath, cb) => {
-  fs.readdir(dirpath, (_error1, filenames) => {
-    const readFileStat = (items, result = []) => {
-      // if (items.length === 0) {
-      //   // Обработку ошибок пока не рассматриваем
-      //   cb(null, result);
-      //   return;
-      // }
-      const [first, ...rest] = items;
-      const filepath = path.join(dirpath, first);
-      fs.stat(filepath, (_error2, stat) => {
-        readFileStat(rest, [...result, { filename: first, size: stat.size }]);
-      });
-    };
-    readFileStat(filenames);
-  });
-};
-
-const write = (filepath, content, cb) => {
-  fs.writeFile(filepath, content, (err, data) => {
-    if (err) throw err;
-    cb(null, data);
-  });
-};
-
 const getFileSize = (filepath, cb) => {
   fs.stat(filepath, (_error, stat) => {
-    cb(null, stat.size);
+    cb(null, stat);
+    console.log(stat);
   });
 };
 
 const compareFileSizes = (filepath1, filepath2, cb) => {
-  const file1Size = fs.stat(filepath1, (_error1, stat) => {
-    // console.log(stat.size);
+  fs.stat(filepath1, (_error1, data1) => {
+    // console.log(data1.size);
+    fs.stat(filepath2, (_error2, data2) => {
+      console.log(`${data1}${data2}`);
+    });
   });
-  const file2Size = fs.stat(filepath2, (_error2, stat) => {
-    // console.log(stat.size);
-  });
-  const result = Math.sign(file1Size - file2Size);
 };
 
-// const pathsToCheck = ['./testFile1.txt', './testFile1.txt'];
-const file1 = './myfile';
-const file2 = './temp.js';
-
-// compareFileSizes(file1, file2);
-
-// compareFileSizes(file2, file1, (_err, result) => console.log(result));
-
-// write('./myfile', 'input data', () => {
-//   console.log('success');
-// });
-
-getFileSize(file1, (_err, res) => {
-  console.log(res);
-});
-
-getFileSize(file2, (_err, res) => res);
-
-// const cdgetFileSize = (filepath, cb) => {
-//   fs.stat(filepath, (_error, stat) => {
-//     cb(null, stat.size);
-//   });
-// };
-
-const compare = (filepath1, filepath2, cb) => {
-  cb(null, Math.sign(
-    (getFileSize(filepath1, (_err, res) => res)) - (getFileSize(filepath2, (_err, res) => res)),
-  ));
-};
-
-compare(file1, file2, (_err, result) => console.log(result));
+compareFileSizes('__fixtures__/file1.txt', '__fixtures__/file2.txt', (_err, result) => console.log(result));
+// getFileSize(('__fixtures__/file1.txt', (_err, result) => console.log(result)));
+getFileSize('__fixtures__/file1.txt', (null, result)) => console.log(result);
--- asynchronous_programming/moveFile/src/moveFile.js
+++ asynchronous_programming/moveFile/src/moveFile.js
@@ -1,48 +1,19 @@
 import * as fs from 'fs';
 
-const write = (filepath, content, cb) => {
-  fs.writeFile(filepath, content, (err, data) => {
-    if (err) throw err;
-    cb(null, data);
+const move = (from, to, cb) => {
+  fs.readFile(from, 'utf-8', (error1, data) => {
+    if (error1) {
+      cb(error1);
+      return;
+    }
+    fs.writeFile(to, data, (error2) => {
+      if (error2) {
+        cb(error2);
+        return;
+      }
+      fs.unlink(from, cb);
+    });
   });
 };
 
-const getFileData = (filepath, cb) => fs.readFile(filepath, 'utf-8', (error1, data) => {
-  if (error1) {
-    console.log('error in first file');
-  }
-  cb(null, data.toString());
-});
-
-getFileData('./myfile', (err, data) => {});
-
-// const move = (pathFrom, pathTo, cb) => {
-//   getFileData(pathFrom, (err1, data) => {
-//     if (err1) {
-//       console.log('error in second file');
-//       return;
-//     }
-//     cb(null, data);
-//   });
-//   fs.writeFile('./fileCopy', 'utf-8', (error2, data2) => {
-//     if (error2) {
-//       console.log('error in second file');
-//       return;
-//     }
-//     fs.writeFile('./new-file', `${data1}${data2}`, (error3) => {
-//       if (error3) {
-//         console.log('error during writing');
-//         return;
-//       }
-//       console.log('finished!');
-//     });
-//   });
-// };
-
-// move('/opt/myfile', '/tmp/newfile', (error) => {
-//   if (error) {
-//     console.log('oops');
-//     return;
-//   }
-//   console.log('yes!');
-// });
+export default move;
