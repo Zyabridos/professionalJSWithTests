ae53bca3222e440f45e8dda36e00200331f31fe9
Nina Zyabrina <zyabrina95@gmail.com>
2024-03-05 22:19:27 +0100

    extractor

---
+++ DOM_API/navigation/public/extractor.js
@@ -0,0 +1,6 @@
+export default (root) => {
+  const children = Array.from(root.parentNode.body.children);
+  return children
+    .filter((element) => element.tagName === 'P')
+    .map((element) => element.innerHTML.trim());
+};
